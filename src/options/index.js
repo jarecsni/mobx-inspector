af7e8e7418785712ba546ffdef26f2d1eaa5df39
Janos <janos@iMac.local>
2019-07-06 16:18:27 +0100

    Initial React based project setup

---
+++ src/options/index.js
@@ -0,0 +1,8 @@
+import React from 'react'; // eslint-disable-line
+import ReactDOM from 'react-dom';
+
+const Index = () => (
+  <div>Options Contents</div>
+);
+
+ReactDOM.render(Index, document.getElementById('root'));
---
+++ src/tab/index.js
@@ -0,0 +1,8 @@
+import React from 'react'; // eslint-disable-line
+import ReactDOM from 'react-dom';
+
+const Index = () => (
+  <div>Tab Contents</div>
+);
+
+ReactDOM.render(Index, document.getElementById('root'));
